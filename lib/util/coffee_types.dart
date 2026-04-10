912ffa6e6d6b99208f1f532a102e816a162842cf
moussa <moussaabesse@gmail.com>
2026-04-10 12:56:11 +0100

    first

--- lib/util/coffee_types.dart
+++ lib/util/coffee_types.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors
+// ignore_for_file: prefer_const_constructors_in_immutables, non_constant_identifier_names, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
 
